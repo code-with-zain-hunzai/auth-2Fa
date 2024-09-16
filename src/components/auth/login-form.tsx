2afa081ad8837ec4e7e5f1a823a676acf6db7be7
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-09-16 22:55:37 +0500

    update the button

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,16 +1,55 @@
-import React from 'react'
-import { CardWrapper } from './card-wrapper'
+"use client"
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CardWrapper } from './card-wrapper';
+import { LoginSchema } from "../../../schemas";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const LoginForm = () => {
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
     return (
         <CardWrapper
             headerLabel="Welcome back"
             backButtonLabel="Don't have an account?"
             backButtonHref='/auth/register'
-            showSocial>
-            Login Form
+            showSocial
+        >
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(() => { })} className="space-y-6">
+                    <div className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} placeholder="zaynhunzai15@gmail.com" />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                </form>
+            </Form>
         </CardWrapper>
-    )
-}
+    );
+};
 
-export default LoginForm
+export default LoginForm;
