978524e911eb43dbec102df1c1bab8be1f1e314e
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-09-17 16:20:09 +0500

    create a register Form

--- schemas/index.ts
+++ schemas/index.ts
@@ -6,3 +6,17 @@ export const LoginSchema = z.object({
     }),
     password: z.string().min(6, { message: "Password must be at least 6 characters" }).optional()
 })
+
+
+export const RegisterSchema = z.object({
+    email: z.string().email({
+        message: "Email is required"
+    }),
+    password: z.string().min(6,
+        {
+            message: "Password must be at least 6 characters"
+        }),
+    name: z.string().min(3, {
+        message: "Name is required"
+    }),
+})
