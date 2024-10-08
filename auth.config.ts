ebae1223e41439bbb237945272ce24a8a44e6eab
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-10-08 12:14:49 +0500

    some changes occur in middleware

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,35 @@
-import GitHub from "next-auth/providers/github"
-import type { NextAuthConfig } from "next-auth"
- 
-export default { providers: [GitHub] } satisfies NextAuthConfig
+import bcrypt from "bcryptjs";
+import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "./schemas";
+import { getUserByEmail } from "./data/user";
+
+export default {
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const validatedFields = LoginSchema.safeParse(credentials);
+
+                if (validatedFields.success) {
+                    const { email, password } = validatedFields.data;
+
+                    const user = await getUserByEmail(email);
+                    
+                    // Check if user and password exist, and if the password is defined
+                    if (!user || typeof user.password !== 'string') {
+                        return null;
+                    }
+
+                    // Compare the provided password with the stored hash
+                    const passwordsMatch = await bcrypt.compare(
+                        password,
+                        user.password
+                    );
+
+                    if (passwordsMatch) return user;
+                }
+                return null;
+            }
+        })
+    ]
+} satisfies NextAuthConfig;
--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,10 +1,17 @@
-import { auth } from "../../../../auth"
+import { auth, signOut } from "../../../../auth"
 
 const settingsPage = async () => {
     const session = await auth();
     return (
         <div className="text-center">
             {JSON.stringify(session)}
+            <form action={async () => {
+                "use server"
+
+                await signOut();
+            }}>
+                <button>Sign Out</button>
+            </form>
         </div>
     )
 }
