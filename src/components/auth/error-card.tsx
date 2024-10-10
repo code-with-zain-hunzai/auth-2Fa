e892469e20c27ce8688d2dd1ee3f6cf70251a593
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-10-10 11:08:31 +0500

    add the Github & Google providers

--- src/components/auth/error-card.tsx
+++ src/components/auth/error-card.tsx
@@ -1,21 +1,16 @@
-import { Header } from "@/components/auth/header";
-import { BackButton } from "@/components/auth/back-button";
-import {
-    Card,
-    CardFooter,
-    CardHeader
-} from "@/components/ui/card"
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
 
 export const ErrorCard = () => {
     return (
-        <Card>
-            <CardHeader>
-                <Header label="Oops! somethings went wrong!" />
-            </CardHeader>
-            <CardFooter>
-                <BackButton label="Back to login"
-                href="/auth/login"/>
-            </CardFooter>
-        </Card>
+        <CardWrapper
+        headerLabel="Oops! Somethings went wrong"
+        backButtonHref="/auth/login"
+        backButtonLabel="Back to login"
+        >
+            <div className="w-full flex justify-center">
+                <ExclamationTriangleIcon className="text-destructive"/>
+            </div>
+        </CardWrapper>
     );
 };
--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -2,6 +2,7 @@
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useSearchParams } from "next/navigation";
 import { CardWrapper } from './card-wrapper';
 import { LoginSchema } from "../../../schemas";
 import {
@@ -20,6 +21,8 @@ import { Login } from "../../../actions/login";
 import { useState, useTransition } from "react";
 
 const LoginForm = () => {
+    const searchParams = useSearchParams();
+    const urlError = searchParams.get("error") === "OAuthAccountNotLinked" ? "Email already in use with different provider!" : "";
     const [error, setError] = useState<string | undefined>("")
     const [success, setSuccess] = useState<string | undefined>("")
     const [isPending, startTransition] = useTransition()
@@ -39,14 +42,10 @@ const LoginForm = () => {
         startTransition(() => {
             Login(values)
                 .then((data) => {
-                    if (data) {
-                        setError(data.error || "");
-                        setSuccess(data.success || "");
-                    } 
+                        setError(data?.error);
+                        // Todo: Add when we add 2fa
+                        // setSuccess(data?.success);
                 })
-                .catch((err) => {
-                    setError("An unexpected error occurred");
-                });
         })
     }
 
@@ -89,7 +88,7 @@ const LoginForm = () => {
                             )}
                         />
                     </div>
-                    <FormError message={error} />
+                    <FormError message={error || urlError} />
                     <FormSuccess message={success} />
                     <Button
                         type="submit"
