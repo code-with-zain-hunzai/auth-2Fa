72c5e59dc23fdf1e4e0150b53928da58b96ac114
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-10-11 17:25:23 +0500

    update the email verification process

---
+++ src/components/auth/new-verification-form.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import { CardWrapper } from "./card-wrapper";
+import { useCallback, useEffect, useState } from "react";
+import { BeatLoader } from "react-spinners";
+import { useSearchParams } from "next/navigation";
+import { newVerification } from "../../../actions/new-verification";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
+
+const NewVerificationForm = () => {
+    const [error, setError] = useState<string | undefined>()
+    const [success, setSuccess] = useState<string | undefined>()
+
+    const searchParams = useSearchParams();
+
+    const token = searchParams.get("token")
+
+    const onSubmit = useCallback(() => {
+        if (!token) {
+            setError("Missing Token!");
+            return;
+        }
+
+        newVerification(token)
+            .then((data) => {
+                setSuccess(data.success)
+                setError(data.error)
+            })
+            .catch(() => {
+                setError("Somethings went wrong!")
+            })
+    }, [token])
+
+    useEffect(() => {
+        onSubmit();
+    }, [onSubmit])
+    return (
+        <CardWrapper
+            headerLabel="Confirming your verification"
+            backButtonLabel="Back to login"
+            backButtonHref="/auth/login">
+
+            <div className="flex items-center w-full justify-center">
+                {!success && !error &&
+                    <BeatLoader />}
+
+                <FormSuccess
+                    message={success} />
+                <FormError message={error} />
+            </div>
+        </CardWrapper>
+    )
+}
+
+export default NewVerificationForm
--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -5,12 +5,13 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendVerificationEmail = async (
     email: string,
     token: string) => {
-        const confrimLink = `http://localhost:3000/auth/new-verification?=token${token}`;
+        const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+
 
         await resend.emails.send({
             from:"onboarding@resend.dev",
             to:email,
             subject:"Confrim your email",
-            html:`<p><a href="${confrimLink}">here</a> to confrim email</p>`
+            html:`<p><a href="${confirmLink}">here</a> to confrim email</p>`
         })
 }
