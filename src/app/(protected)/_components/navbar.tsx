705b7f000b1557471bd370d771c81a6ed8af8c03
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-10-29 13:00:05 +0500

    Add User Button

--- src/app/(protected)/_components/navbar.tsx
+++ src/app/(protected)/_components/navbar.tsx
@@ -3,47 +3,48 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@/components/auth/user-button";
 
 export const Navbar = () => {
-  const pathname = usePathname();
+    const pathname = usePathname();
 
-  return (
-    <nav className="bg-secondary flex justify-between items-center p-4 rounded-xl w-[600px] shadow-sm">
-      <ul className="flex gap-x-2">
-        <li>
-          <Button
-            asChild
-            variant={pathname === "/server" ? "default" : "outline"}
-          >
-            <Link href="/server">Server</Link>
-          </Button>
-        </li>
-        <li>
-          <Button
-            asChild
-            variant={pathname === "/client" ? "default" : "outline"}
-          >
-            <Link href="/client">Client</Link>
-          </Button>
-        </li>
-        <li>
-          <Button
-            asChild
-            variant={pathname === "/admin" ? "default" : "outline"}
-          >
-            <Link href="/admin">Admin</Link>
-          </Button>
-        </li>
-        <li>
-          <Button
-            asChild
-            variant={pathname === "/settings" ? "default" : "outline"}
-          >
-            <Link href="/settings">Settings</Link>
-          </Button>
-        </li>
-      </ul>
-      <p>User Action</p>
-    </nav>
-  );
+    return (
+        <nav className="bg-secondary flex justify-between items-center p-4 rounded-xl w-[600px] shadow-sm">
+            <ul className="flex gap-x-2">
+                <li>
+                    <Button
+                        asChild
+                        variant={pathname === "/server" ? "default" : "outline"}
+                    >
+                        <Link href="/server">Server</Link>
+                    </Button>
+                </li>
+                <li>
+                    <Button
+                        asChild
+                        variant={pathname === "/client" ? "default" : "outline"}
+                    >
+                        <Link href="/client">Client</Link>
+                    </Button>
+                </li>
+                <li>
+                    <Button
+                        asChild
+                        variant={pathname === "/admin" ? "default" : "outline"}
+                    >
+                        <Link href="/admin">Admin</Link>
+                    </Button>
+                </li>
+                <li>
+                    <Button
+                        asChild
+                        variant={pathname === "/settings" ? "default" : "outline"}
+                    >
+                        <Link href="/settings">Settings</Link>
+                    </Button>
+                </li>
+            </ul>
+            <UserButton />
+        </nav>
+    );
 };
---
+++ src/components/auth/user-button.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+import {
+    Avatar,
+    AvatarImage,
+    AvatarFallback
+} from "@/components/ui/avatar";
+import { FaUser } from 'react-icons/fa';
+import { useCurrentUser } from '../../../hooks/use-current-user';
+import { LogoutButton } from './logout-button';
+import { ExitIcon } from '@radix-ui/react-icons';
+
+export const UserButton = () => {
+    const user = useCurrentUser();
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger className='outline-none'>
+                <Avatar>
+                    <AvatarImage src={user?.image || ""} />
+                    <AvatarFallback className='bg-purple-500'>
+                        <FaUser className='text-white' />
+                    </AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='w-40 ' align='end' >
+                <LogoutButton>
+                    <DropdownMenuItem>
+                        <ExitIcon />
+                        Logout
+                    </DropdownMenuItem>
+                </LogoutButton>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
