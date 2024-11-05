73bd9708855f3374487658b2503f1b06b147f334
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-11-05 19:28:19 +0500

    few changes in code

--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -46,7 +46,7 @@ const AdminPage = () => {
                 </RoleGate>
                 <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
                     <p className="text-sm font-medium">
-                        Admin-only Api Route
+                        Admin Only Api Route
                     </p>
                     <Button onClick={onApiRouteClick}>
                         Click to test
@@ -54,7 +54,7 @@ const AdminPage = () => {
                 </div>
                 <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
                     <p className="text-sm font-medium">
-                        Admin-only Server Action
+                        Admin Only Server Action
                     </p>
                     <Button onClick={onServerActionClick}>
                         Click to test
