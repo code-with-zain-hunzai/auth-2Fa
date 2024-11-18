10500ff2ae047f34dfaad1e86b0864036b2478cc
zain <dev.zainulabbdin@gmail.com>
2024-11-18 13:42:28 +0500

    few changes in settingSchemas and setting page

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,8 +1,12 @@
 "use client"
 
-
-import { useTransition } from "react"
+import * as z from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useState, useTransition } from "react"
 import { useSession } from "next-auth/react"
+
+import { SettingsSchema } from "../../../../schemas"
 import {
     Card,
     CardHeader,
@@ -10,20 +14,48 @@ import {
 } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { settings } from "../../../../actions/setting"
+import {
+    Form,
+    FormField,
+    FormControl,
+    FormItem,
+    FormLabel,
+    FormDescription,
+    FormMessage
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { useCurrentUser } from "../../../../hooks/use-current-user"
+import { FormSuccess } from "@/components/form-success"
+import { FormError } from "@/components/form-error"
 
 const settingsPage = () => {
+    const user = useCurrentUser()
+    const [error, setError] = useState<string | undefined>();
+    const [success, setSuccess] = useState<string | undefined>();
+
     const { update } = useSession();
     const [isPending, startTransition] = useTransition();
 
+    const form = useForm<z.infer<typeof SettingsSchema>>({
+        resolver: zodResolver(SettingsSchema),
+        defaultValues: {
+            name: user?.name || undefined
+        }
+    });
 
-    const onClick = () => {
+    const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
         startTransition(() => {
-            settings({
-                name: "New Name!"
-            })
-                .then(() => {
-                    update();
+            settings(values)
+                .then((data) => {
+                    if (data.error) {
+                        setError(data.error)
+                    }
+                    if (data.success) {
+                        update()
+                        setSuccess(data.success)
+                    }
                 })
+                .catch(() => setError("Somethings went wrong!"))
         });
 
     }
@@ -36,11 +68,35 @@ const settingsPage = () => {
                 </p>
             </CardHeader>
             <CardContent>
-                <Button
-                    disabled={isPending}
-                    onClick={onClick}>
-                    Update name
-                </Button>
+                <Form {...form}>
+                    <form
+                        className="space-y-6"
+                        onSubmit={form.handleSubmit(onSubmit)}>
+                        <div className="space-y-4">
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => {
+                                    return <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                {...field}
+                                                placeholder="zain"
+                                                disabled={isPending} />
+                                        </FormControl>
+                                    </FormItem>
+                                }} />
+                        </div>
+                        <FormError message={error} />
+                        <FormSuccess message={success} />
+                        <Button
+                            disabled={isPending}
+                            type="submit">
+                            Save
+                        </Button>
+                    </form>
+                </Form>
             </CardContent>
         </Card>
     )
